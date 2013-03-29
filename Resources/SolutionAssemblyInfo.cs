8f15cf617133fe1f3f952f3bb318d000c545d68b
2a <2a@486f72.com>
2013-03-29 11:30:44 +0200

    Removed needless attribute

--- Resources/SolutionAssemblyInfo.cs
+++ Resources/SolutionAssemblyInfo.cs
@@ -4,4 +4,3 @@
 [assembly: AssemblyProduct("Mega client for .NET")]
 [assembly: AssemblyCopyright("Copyright © 2a486f72 2013")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
